e7832ee6cc8726682f487085c34fab80b7c98c2e
Marco Pacini <pacinim88@gmail.com>
2016-10-19 10:48:23 +0100

    Added static GetProperties method and relative test unit.

---
+++ INI/INIReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INI
+{
+    public class INIReader
+    {
+        public static Dictionary<string, string> GetProperties(string filepath)
+        {
+            var data = new Dictionary<string, string>();
+
+            foreach (var row in File.ReadAllLines(filepath))
+                data.Add(row.Split('=')[0], string.Join("=", row.Split('=').Skip(1).ToArray()));
+
+            return data;
+        }
+    }
+}
---
+++ INIReaderTests/INIReaderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using INI;
+
+namespace INITests
+{
+    [TestClass]
+    public class INIReaderTest
+    {
+        [TestMethod]
+        public void GetProperties_Test()
+        {
+            var filepath = "properties.ini";
+            var properties = INIReader.GetProperties(filepath);
+
+            Assert.AreEqual(properties["host"], "localhost");
+            Assert.AreEqual(properties["user"], "admin");
+            Assert.AreEqual(properties["password"], "qwerty");
+        }
+    }
+}
